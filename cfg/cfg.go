5fc37d19e6238071ad64afb49202d99fe7f04687
thatpix3l <git@thatpix3l.com>
2022-05-31 13:58:04 -0400

    Add config Keys comments

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -3,12 +3,12 @@ package cfg
 import "strconv"
 
 type Keys struct {
-	VmcListenIP          string
-	VmcListenPort        int
-	WebListenIP          string
-	WebListenPort        int
-	ModelUpdateFrequency int
-	ConfigPath           string
+	VmcListenIP          string // IP address to listen for VMC data
+	VmcListenPort        int    // Port to listen for VMC data
+	WebListenIP          string // IP address to serve the frontend
+	WebListenPort        int    // Port to serve the frontend
+	ModelUpdateFrequency int    // Times per second to send the live model data to frontend clients
+	ConfigPath           string // Path to config file
 }
 
 func (k Keys) GetVmcSocketAddress() string {
